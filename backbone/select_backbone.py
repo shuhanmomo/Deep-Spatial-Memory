17606b436ac6630baeb95d0d6fbd51993f7d8201
Tracy Miao <tracymiao98@gmail.com>
2023-11-17 18:55:50 -0500

    add simplified resnet3d

--- backbone/select_backbone.py
+++ backbone/select_backbone.py
@@ -1,16 +1,23 @@
-from .resnet_2d3d import * 
+from .resnet_2d3d import *
 
-def select_resnet(network,):
-    param = {'feature_size': 1024}
-    if network == 'resnet18':
+
+def select_resnet(
+    network,
+):
+    param = {"feature_size": 1024}
+    if network == "resnet18":
         model = resnet18_2d3d_full(track_running_stats=True)
-        param['feature_size'] = 256
-    elif network == 'resnet34':
+        param["feature_size"] = 256
+    elif network == "resnet34":
         model = resnet34_2d3d_full(track_running_stats=True)
-        param['feature_size'] = 256 
-    elif network == 'resnet50':
+        param["feature_size"] = 256
+    elif network == "resnet50":
         model = resnet50_2d3d_full(track_running_stats=True)
-    else: 
+
+    elif network == "resnet18_simplified":
+        model = resnet18_2d3d_simplified(track_running_stats=True)
+        param["feature_size"] = 128
+    else:
         raise NotImplementedError
 
     return model, param
